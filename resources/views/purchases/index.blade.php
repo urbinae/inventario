766e189dee1890c039654768e7271ca12e1918e7
Eimar Urbina <eimar@e-abclearning.com>
2020-08-21 00:32:53 -0400

    cambio de boton

--- resources/views/purchases/index.blade.php
+++ resources/views/purchases/index.blade.php
@@ -16,6 +16,8 @@
                     <div class="panel-group" id="accordion">
                         @foreach($lotes as $lote)
                         <div class="panel panel-default">
+                            <a href="#" class="btn btn-success btn-sm pull-right">Excel</a>
+
                             <div class="panel-heading">
                                 <h4 class="panel-title">
                                     <a data-toggle="collapse" data-parent="#accordion" href="#lote-{{$lote->id}}">{{$lote->name}}</a>
@@ -28,21 +30,39 @@
                                             <thead>
                                                 <tr>
                                                     <th>Producto</th>
-                                                    <th>Precio Unitario</th>
                                                     <th>Cantidad</th>
-                                                    <th>Total</th>
-                                                    <th>Fecha</th>
+                                                    <th>Costo Unitario</th>
+                                                    <th>Costo por Cantidad</th>
+                                                    <th>Costo Fijo de Flete</th>
+                                                    <th>Costo Variablede Flete</th>
+                                                    <th>Costo total</th>
+                                                    <th>Ganancia del 30%</th>
+                                                    <th>Costo Total Unitario</th>
+                                                    <th>Costo Total Ganancia 20%</th>
                                                 </tr>
                                             </thead>
                                             <tbody>
                                                 @foreach($purchases as $purchase)
+                                                    @php
+                                                    $cost_fijo_flete = round($purchase->lote->costo_fijo_usd/count($purchases), 2);
+                                                    $cost_variable_flete = round($purchase->lote->costo_variable_usd/count($purchases), 2);
+                                                    $costo_total = $purchase->cost + $purchase->lote->costo_fijo_usd + $purchase->lote->costo_variable_usd;
+                                                    $ganancia30 = ($purchase->cost + $purchase->lote->costo_fijo_usd + $purchase->lote->costo_variable_usd)*0.3;
+                                                    $costo_total_unitario = $costo_total + $ganancia30;
+                                                    $costo_total20 = $costo_total*0.2;
+                                                    @endphp
                                                 @if($purchase->lote_id == $lote->id)
                                                 <tr>
                                                     <td>{{$purchase->product->name}}</td>
-                                                    <td>{{$purchase->price}}</td>
                                                     <td>{{$purchase->cant}}-{{$purchase->unity}}</td>
-                                                    <td>{{$purchase->cost}}</td>
-                                                    <td>{{$purchase->created_at}}</td>
+                                                    <td>{{$purchase->price}}</td>
+                                                    <td>{{$purchase->cost}}</td>                                                    
+                                                    <td>@php echo $cost_fijo_flete @endphp</td>                                                    
+                                                    <td>@php echo $cost_variable_flete @endphp</td>
+                                                    <td>@php echo $costo_total @endphp </td>
+                                                    <td>@php echo $ganancia30 @endphp</td>
+                                                    <td>@php echo $costo_total_unitario @endphp</td>
+                                                    <td>@php echo $costo_total20 @endphp</td>
                                                     <td>
                                                         @can('purchases.show')
                                                         <a href="{{route('purchases.show', $purchase->id)}}" class="btn btn-sm btn-default">
